a5b6fbe5ffc45af92b7779644dc374a5becda167
zeromaker-coder <1849059209@qq.com>
2025-06-28 15:30:57 +0800

    加入最长白列寻边线，发现大津法函数中的重大错误

--- SMARTCAR/project/code/image.c
+++ SMARTCAR/project/code/image.c
@@ -1,7 +1,55 @@
 #include "image.h"
 
+//需要处理的图像的长宽
 #define DEAL_IMAGE_H 120
-#define DEAL_IMAGE_W 160
+#define DEAL_IMAGE_W 188
+
+//一些常用变量
+uint8 binary_image[MT9V03X_H][MT9V03X_W]; //二值化图像数组
+uint8 left_line[MT9V03X_H];//左边界数组
+uint8 right_line[MT9V03X_H];//右边界数组
+uint8 mid_line[MT9V03X_H];//中线数组
+uint8 white_count[MT9V03X_W];//每一列的白列长度
+uint8 search_stop_line;//搜索截止行
+uint8 boundary_start_left;//左边界起点
+uint8 boundary_start_right;//右边界起点
+int16 longest_white_left[2];//左边的最长白列，0为长度，1为下标
+int16 longest_white_right[2];//右边的最长白列，0为长度，1为下标
+uint8 left_lost_flag[MT9V03X_H];//左丢线数组
+uint8 right_lost_flag[MT9V03X_H];//右丢线数组
+uint8 left_lost_count;//左丢线计数
+uint8 right_lost_count;//右丢线计数
+uint8 left_right_lost_count;//左右同时丢线计数
+
+
+
+
+
+/**
+* @brief   出界判断,选取图像底部中间10*3区域进行判断
+* @param   *in_image  原始图像
+* @retval   0:正常 1:出界
+*/
+void image_out_of_bounds(unsigned char in_image[DEAL_IMAGE_H][DEAL_IMAGE_W])
+{
+    int sum=0;
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            sum+=in_image[DEAL_IMAGE_H-1-j][DEAL_IMAGE_W/2-5+i];
+        }
+    }
+    int average = sum / 30; // 计算平均值
+    if(average<50)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 /**
 * @brief   图像压缩
@@ -34,7 +82,7 @@ void Image_Compress(unsigned char *in_image, unsigned char *out_image)
   */
 uint8 otsu_get_threshold(uint8 *image, uint16 width, uint16 height) {
     uint32 histogram[256] = {0};
-    uint32 total_pixels = width * height;
+    uint32 total_pixels = width * height/4;
 
     // 计算直方图
     for (uint16 i = 0; i < height; i+=2) {
@@ -140,3 +188,109 @@ void image_draw_black(unsigned char image[DEAL_IMAGE_H][DEAL_IMAGE_W])
     }
 }
 
+
+/**
+*
+* @brief  图像扫线，根据网上的教程选用双最长白列
+* @param  
+*
+**/
+void longest_white_sweep_line(uint8 image[DEAL_IMAGE_H][DEAL_IMAGE_W])
+{
+    int i,j;
+    int start_point = 20;//起搜下标
+    int end_point = DEAL_IMAGE_W - 20;//结束下标
+    int left_border = 0, right_border = 0;//存储赛道临时位置
+    //变量清零
+    longest_white_left[0] = 0;
+    longest_white_left[1] = 0;
+    longest_white_right[0] = 0;
+    longest_white_right[1] = 0;
+    boundary_start_left=0;
+    boundary_start_right=0;
+    for(i=0;i<DEAL_IMAGE_H-1;i++)
+    {
+        left_line[i]=0;
+        right_line[i]=DEAL_IMAGE_W-1;
+        left_lost_flag[i]=0;
+        right_lost_flag[i]=0;
+    }
+    for(i=0;i<DEAL_IMAGE_W-1;i++)
+    {
+        white_count[i]=0;
+    }
+    //记录每列白点数量
+    for(j=start_point;j<end_point;j++)
+    {
+        for(i=DEAL_IMAGE_H-1;i>=0;i--)
+        {
+            if(image[i][j] == 1)break;//黑点跳出
+            else white_count[j]++;//白点计数
+        }
+    }
+    //寻找左最长白列
+    for(i=start_point;i<end_point;i++)
+    {
+        if( longest_white_left[0] < white_count[i])//找最长的那一列，寻到右边界
+        {
+            longest_white_left[0] = white_count[i];
+            longest_white_left[1] = white_count[i];
+        }
+    }
+    //寻找右最长白列
+    for(i=end_point;i>=longest_white_left[1];i--)//从右往左，找到右最长白列，寻到左边最长白列位置
+    {
+        if( longest_white_right[0] < white_count[i])//找最长的那一列
+        {
+            longest_white_right[0] = white_count[i];
+            longest_white_right[1] = white_count[i];
+        }
+    }
+    
+    search_stop_line = longest_white_left[0];//非常重要，搜索截止行存储
+
+    //巡线
+    for(i = DEAL_IMAGE_H-1;i>=DEAL_IMAGE_H-search_stop_line;i--)
+    {
+        for(j = longest_white_right[1]; j<=DEAL_IMAGE_W-1-2;j++)//从右最长白列找右边
+        {
+            if(image[i][j] == 1 && image[i][j+1] == 0 && image[i][j+2] == 0)//白黑黑，找到右边界
+            {
+                right_border = j;
+                right_lost_flag[i]=0;
+                break;
+            }
+            else if(j>=DEAL_IMAGE_W-1-2)//右边界丢失
+            {
+                right_border = j;
+                right_lost_flag[i]=1;//右边界丢线记录
+                break;
+            }
+        }
+        for(j = longest_white_left[1]; j>=2;j--)//从左最长白列找左边
+        {
+            if(image[i][j] == 1 && image[i][j-1] == 0 && image[i][j-2] == 0)//白黑黑，找到右边界
+            {
+                left_border = j;
+                left_lost_flag[i]=0;
+                break;
+            }
+            else if(j<=2)//左边界丢失
+            {
+                left_border = j;
+                left_lost_flag[i]=1;//左边界丢线记录
+                break;
+            }
+        }
+        left_line[i]=left_border;//存储左边线
+        right_line[i]=right_border;//存储右边线
+        
+    }
+    //记录丢边情况
+    for(i=DEAL_IMAGE_H-1;i>=0;i--)
+    {
+        if(left_lost_flag[i]==1&&right_lost_flag[i]==0)left_lost_count++;//左丢
+        if(left_lost_flag[i]==0&&right_lost_flag[i]==1)right_lost_count++;//右丢
+        if(left_lost_flag[i]==1&&right_lost_flag[i]==1)left_right_lost_count++;//丢双边
+    }
+}
